5b3984fc8f08bd7bc60b1eb77681e838f5a3b103
Anton Cherednichenko <anton@anton.eco>
2024-04-18 15:18:09 +0200

    🔧 Add OpenAI Mistral configuration and HTTP macro
    A new configuration file `mistral.php` has been added to provide settings for OpenAI Mistral API operations. It allows specification of API key, system prompt, and request timeout. Additionally, an HTTP macro `mistral` is added in `AppServiceProvider.php` for setting headers and baseUrl for future HTTP requests.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('generate-ics', function (User $user){
+        Gate::define('generate-ics', function (User $user) {
 
             if ($user->credits = 0) {
                 Response::deny("You don't have enough credits");
@@ -33,6 +34,14 @@ class AppServiceProvider extends ServiceProvider
             }
 
             Response::allow();
+        });
+
+        Http::macro('mistral', function () {
+            return Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . config('mistral.api_key')
+            ])->baseUrl('https://api.mistral.ai/v1/');
         });
     }
 }
