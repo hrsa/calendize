c90cc37c593d43d174fcebe1c2e99e0701468ff6
Anton Cherednichenko <anton@anton.eco>
2024-04-21 22:55:16 +0200

    Dependencies update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-defineConfig({
+export default defineConfig({
     server: {
         hmr: {
             host: 'localhost'
