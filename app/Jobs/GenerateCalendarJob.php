3df76426a1a6236a7929ec6ee01d9f2d0624f7d9
Anton Cherednichenko <anton@anton.eco>
2024-04-22 00:28:23 +0200

    🔄 Update GenerateCalendarJob for retry and methods separation
    Include a retry mechanism if the OpenAI call fails. Separated the logic for calling OpenAI and Mistral into their own private methods.

--- app/Jobs/GenerateCalendarJob.php
+++ app/Jobs/GenerateCalendarJob.php
@@ -4,13 +4,18 @@ namespace App\Jobs;
 
 use App\Events\IcsEventProcessed;
 use App\Models\IcsEvent;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI\Responses\Chat\CreateResponse;
+use stdClass;
 
 class GenerateCalendarJob implements ShouldQueue
 {
@@ -18,20 +23,14 @@ class GenerateCalendarJob implements ShouldQueue
 
     public int $tries = 5;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct(public icsEvent $icsEvent)
     {
     }
 
-    /**
-     * Execute the job.
-     */
     public function handle(): void
     {
+        $result = null;
         $systemPrompt = config('openai.system_prompt');
-
         $now = Carbon::now();
         $systemPrompt .= " As of today, the date is {$now}.";
 
@@ -39,46 +38,73 @@ class GenerateCalendarJob implements ShouldQueue
             $systemPrompt .= "User's timezone: " . $this->icsEvent->timezone;
         }
 
+
         try {
-            $result = OpenAI::chat()->create([
-                'model' => 'gpt-4-turbo-2024-04-09',
-                'messages' => [
-                    ['role' => 'system', 'content' => $systemPrompt],
-                    ['role' => 'user', 'content' => $this->icsEvent->prompt],
-                ],
-                'max_tokens' => 2800,
-                'response_format' => ['type' => 'json_object']
-            ]);
-        } catch (\Exception $e) {
-            $this->icsEvent->update(['error' => "I'm sorry, my servers are having hiccups. Please try again in 30-60 minutes!"]);
-            IcsEventProcessed::dispatch($this->icsEvent);
-            $this->fail($e);
+            $result = $this->generateOpenAIResponse($systemPrompt);
+        } catch (Exception $e) {
+            Log::alert("OpenAI error generating IcsEvent #{$this->icsEvent->id}");
         }
 
-        $reply = $result?->choices[0]->message->content;
+        if (!$result) {
+            try {
+                $result = $this->generateMistralResponse($systemPrompt);
+            } catch (Exception $e) {
+                $this->icsEvent->update(['error' => "I'm sorry, my servers are having hiccups. Please try again in 30-60 minutes!"]);
+                IcsEventProcessed::dispatch($this->icsEvent);
+                $this->fail($e);
+            }
+        }
 
-        if (!$reply) {
+        $jsonIcs = $result?->choices[0]->message->content;
+
+        if (!$jsonIcs) {
             $this->release(300);
         }
 
-        $decodedReply = json_decode($reply, true);
-
-        if (array_key_exists('error', $decodedReply)) {
+        $decodedReply = json_decode($jsonIcs, true);
+        if (array_key_exists('ics', $decodedReply) && isset($decodedReply['ics'])) {
+            $this->icsEvent->update(['ics' => $decodedReply['ics']]);
+            $this->icsEvent->user->decrement('credits');
+        } else {
             $this->icsEvent->update(['error' => $decodedReply['error']]);
             $this->icsEvent->user->increment('failed_requests');
 
             if ($this->icsEvent->user->failed_requests >= $this->icsEvent->user->credits) {
                 $this->icsEvent->user->decrement('credits');
             }
-        } else {
-            $this->icsEvent->update(['ics' => $decodedReply['ics']]);
-            $this->icsEvent->user->decrement('credits');
         }
 
-        ray('Got a reply', $reply)->green();
-        ray($result->usage->totalTokens)->blue();
-        $this->icsEvent->update('token_usage', $result->usage->totalTokens);
+
+        $tokenUsage = $result->usage?->totalTokens ?? $result->usage->total_tokens;
+        $this->icsEvent->update(['token_usage' => $tokenUsage]);
         IcsEventProcessed::dispatch($this->icsEvent);
-        ray('broadcasting')->orange();
+    }
+
+    private function generateOpenAIResponse(string $systemPrompt): CreateResponse
+    {
+        return OpenAI::chat()->create([
+            'model' => 'gpt-4-turbo-2024-04-09',
+            'messages' => [
+                ['role' => 'system', 'content' => $systemPrompt],
+                ['role' => 'user', 'content' => $this->icsEvent->prompt],
+            ],
+            'max_tokens' => 2800,
+            'response_format' => ['type' => 'json_object']
+        ]);
+    }
+
+    private function generateMistralResponse(string $systemPrompt): stdClass
+    {
+        $result = Http::mistral()->post("/chat/completions", [
+            "model" => "mistral-large-latest",
+            'messages' => [
+                ['role' => 'system', 'content' => $systemPrompt],
+                ['role' => 'user', 'content' => $this->icsEvent->prompt],
+            ],
+            'max_tokens' => 2800,
+            'response_format' => ['type' => 'json_object']
+        ])->json();
+
+        return json_decode(json_encode($result));
     }
 }
