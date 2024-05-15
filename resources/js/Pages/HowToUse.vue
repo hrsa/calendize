c90775ecf604f981ed94851aa2e84db64ab3e5cd
Anton Cherednichenko <anton@anton.eco>
2024-05-16 00:44:56 +0200

    Add navigation to slider in HowToUse.vue

--- resources/js/Pages/HowToUse.vue
+++ resources/js/Pages/HowToUse.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import {Head} from '@inertiajs/vue3';
+import {Head, router} from '@inertiajs/vue3';
 import {Swiper, SwiperSlide} from "swiper/vue";
 import {Navigation} from 'swiper/modules';
 import type SwiperClass from 'swiper';
@@ -9,6 +9,11 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import {ref} from "vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+
+interface SwiperElement extends HTMLElement {
+    swiper: any;
+}
 
 
 const activeSlide = ref(1);
@@ -18,6 +23,13 @@ const handleProgress = (swiper: SwiperClass) => {
     activeSlide.value = swiper.activeIndex + 1;
 }
 
+const setSlide = (num: number) => {
+    const swiperEl = (document.querySelector('.swiper') as SwiperElement)?.swiper;
+    if (swiperEl) {
+        swiperEl.slideTo(num - 1);
+    }
+}
+
 </script>
 
 <template>
@@ -25,8 +37,8 @@ const handleProgress = (swiper: SwiperClass) => {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="text-center text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">How do i use
-                Calendize?</h2>
+            <h2 class="text-center text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                How do i use Calendize ?</h2>
         </template>
 
         <div class="text-black/50 dark:text-white/50">
@@ -38,26 +50,54 @@ const handleProgress = (swiper: SwiperClass) => {
                             hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3
                             lg:p-10 lg:pb-10 dark:bg-gray-800 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700
                             dark:focus-visible:ring-[#FF2D20]">
-                    <h1 class="text-3xl">{{activeSlide}}</h1>
+                    <div class="flex w-full justify-around">
+                        <div @click="setSlide(1)"
+                             class="rounded-full size-16 flex items-center justify-center cursor-pointer"
+                             :class="activeSlide >= 1 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                            <p class="text-2xl p-8">1</p>
+                        </div>
+                        <hr class="h-1 w-full border-0 rounded-xl place-self-center"
+                            :class="activeSlide >= 2 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                        <div @click="setSlide(2)"
+                             class="rounded-full size-16 flex items-center justify-center cursor-pointer"
+                             :class="activeSlide >= 2 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                            <p class="text-2xl p-8">2</p>
+                        </div>
+                        <hr class="h-1 w-full border-0 rounded-xl place-self-center"
+                            :class="activeSlide >= 3 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                        <div @click="setSlide(3)"
+                             class="rounded-full size-16 flex items-center justify-center cursor-pointer"
+                             :class="activeSlide >= 3 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                            <p class="text-2xl p-8">3</p>
+                        </div>
+                        <hr class="h-1 w-full border-0 rounded-xl place-self-center"
+                            :class="activeSlide >= 4 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                        <div @click="setSlide(4)"
+                             class="rounded-full size-16 flex items-center justify-center cursor-pointer"
+                             :class="activeSlide >= 4 ? 'bg-green-700' : 'bg-gray-400'"
+                        >
+                            <p class="text-2xl p-8">4</p>
+                        </div>
+                    </div>
                     <div class="h-full lg:min-w-[40rem] lg:h-full flex-1">
                         <swiper
                             :spaceBetween="0"
                             :centeredSlides="true"
                             :grabCursor="true"
-                            :pagination="{ clickable: true }"
-                            :autoplay="{
-                                          delay: 3500,
-                                          disableOnInteraction: false,
-                                        }"
                             :modules="[Navigation]"
-                            class="h-full mySwiper cursor-hand"
-                            ref="swiperEl"
+                            class="h-full how-to-use-swiper cursor-hand"
                             @slideChange="handleProgress"
                         >
                             <swiper-slide class="flex flex-col flex-grow">
-                                <div class="flex h-full flex-col justify-center gap-6 p-5 md:p-12">
+                                <div class="flex h-full flex-col justify-center gap-4 p-5 md:p-12">
                                     <h2 class="rounded-lg lg:p-6 text-center text-xl font-semibold uppercase tracking-widest text-gray-800 dark:text-gray-200 lg:text-3xl">
-                                        Tours
+                                        Find an event to calendize
                                     </h2>
                                     <img class="mx-auto rounded-xl block lg:hidden"
                                          :src="'/home/usage/tour-mobile.webp'"
@@ -68,22 +108,28 @@ const handleProgress = (swiper: SwiperClass) => {
                                 </div>
                             </swiper-slide>
                             <swiper-slide class="flex flex-col">
-                                <div class="flex h-full flex-col justify-center gap-6 p-5 md:p-12">
+                                <div class="flex h-full flex-col justify-center gap-4 p-5 md:p-12">
                                     <h2 class="rounded-lg lg:p-6 text-center text-xl font-semibold uppercase tracking-widest text-gray-800 dark:text-gray-200 lg:text-3xl">
-                                        Flights
+                                        Send it to me
                                     </h2>
-                                    <img class="mx-auto rounded-xl block lg:hidden"
-                                         :src="'/home/usage/flight-mobile.webp'"
-                                         alt="flight"/>
+                                    <div class="rounded-lg text-center text-lg font-semibold uppercase tracking-widest text-gray-800 dark:text-gray-200 lg:text-xl">
+                                        Forward your email to hey@calendize.it
+                                    </div>
+                                    <div class="rounded-lg text-center text-lg font-extrabold uppercase tracking-widest text-gray-900 dark:text-gray-400 lg:text-2xl">
+                                        or
+                                    </div>
+                                    <div class="rounded-lg text-center text-lg font-semibold uppercase tracking-widest text-gray-800 dark:text-gray-200 lg:text-xl">
+                                        <span class="align-middle">Copy-paste its text here: </span><PrimaryButton @click="router.get(route('generate'))"
+                        class="w-fit m-auto">Generate</PrimaryButton></div>
                                     <img class="mx-auto rounded-xl hidden lg:block"
                                          :src="'/home/usage/flight.webp'"
                                          alt="flight"/>
                                 </div>
                             </swiper-slide>
                             <swiper-slide class="flex flex-col">
-                                <div class="flex h-full flex-col justify-center gap-6 p-5 md:p-12">
+                                <div class="flex h-full flex-col justify-center gap-4 p-5 md:p-12">
                                     <h2 class="rounded-lg lg:p-6 text-center text-xl font-semibold uppercase tracking-widest text-gray-800 dark:text-gray-200 lg:text-3xl">
-                                        Conferences
+                                        Check your email
                                     </h2>
                                     <img class="mx-auto rounded-xl block lg:hidden"
                                          :src="'/home/usage/event-mobile.webp'"
@@ -94,16 +140,16 @@ const handleProgress = (swiper: SwiperClass) => {
                                 </div>
                             </swiper-slide>
                             <swiper-slide class="flex flex-col">
-                                <div class="flex h-full flex-col justify-center gap-6 p-5 md:p-12">
+                                <div class="flex h-full flex-col justify-center gap-4 p-5 md:p-12">
                                     <h2 class="rounded-lg lg:p-6 text-center text-xl font-semibold uppercase tracking-widest text-gray-800 dark:text-gray-200 lg:text-3xl">
-                                        Concerts
+                                        Add it to your calendar!
                                     </h2>
                                     <img class="mx-auto rounded-xl block lg:hidden"
-                                         :src="'/home/usage/concert-mobile.webp'"
-                                         alt="concert"/>
+                                         :src="'/home/usage/tour-mobile-calendized.webp'"
+                                         alt="tour"/>
                                     <img class="mx-auto rounded-xl hidden lg:block"
-                                         :src="'/home/usage/concert.webp'"
-                                         alt="concert"/>
+                                         :src="'/home/usage/tour-calendized.webp'"
+                                         alt="tour"/>
                                 </div>
                             </swiper-slide>
                         </swiper>
