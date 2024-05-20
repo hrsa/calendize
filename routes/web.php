7b1bf0df3f6a1d5f0f6fa7c48262d9ceca61d302
Anton Cherednichenko <anton@anton.eco>
2024-05-20 12:58:39 +0200

    Added RedirectIfNotGuest middleware to simplify routes

--- routes/web.php
+++ routes/web.php
@@ -3,54 +3,39 @@
 use App\Http\Controllers\CalendarGeneratorController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    if (Auth::check()) {
-        return redirect()->route('generate');
-    }
-
-    return Inertia::render('Landing');
-})->name('home');
-
-Route::get('/try', function () {
-    if (Auth::check()) {
-        return redirect()->route('generate');
-    }
-
-    return Inertia::render('Try', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-    ]);
-})->name('try');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::get('/generate', function () {
-        return Inertia::render('Generate', [
-            'serverErrorMessage' => request('serverErrorMessage'),
-            'serverSuccess' => request('serverSuccess'),
-            'eventId' => request('eventId'),
-            'eventSecret' => request('eventSecret'),
-        ]);
-    })->name('generate');
-
-    Route::get('/my-events', [CalendarGeneratorController::class, 'usersEvents'])->name('my-events');
-    Route::get('/how-to-use', fn () => Inertia::render('HowToUse'))->name('how-to-use');
+Route::middleware('if-not-guest-redirect-to:generate')->group(function () {
+    Route::get('/', fn () => Inertia::render('Landing'))->name('home');
+    Route::get('/try', fn () => Inertia::render('Try'))->name('try');
 });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/how-to-use', fn () => Inertia::render('HowToUse'))->name('how-to-use');
+    Route::get('/my-events', [CalendarGeneratorController::class, 'usersEvents'])->name('my-events');
+
+    Route::middleware('verified')->group(function () {
+
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::get('/generate', function () {
+            return Inertia::render('Generate', [
+                'serverErrorMessage' => request('serverErrorMessage'),
+                'serverSuccess' => request('serverSuccess'),
+                'eventId' => request('eventId'),
+                'eventSecret' => request('eventSecret'),
+            ]);
+        })->name('generate');
+    });
 });
 
 Route::get('event/download/{id}/{secret}', [CalendarGeneratorController::class, 'downloadEvent'])->name('event.download');
 
+Route::get('pricing', fn () => Inertia::render('Pricing'))->name('pricing');
 Route::get('privacy-policy', fn () => Inertia::render('PrivacyPolicy'))->name('privacy-policy');
 Route::get('terms-of-service', fn () => Inertia::render('TermsOfService'))->name('terms-of-service');
 
